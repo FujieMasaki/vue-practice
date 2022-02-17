16586ece732c489f68be0b4fcc84f73689f8f790
MasakiFujie <ffffffzzzzzeeee@gmail.com>
2022-02-17 18:57:29 +0900

    論理演算子でのスタイル変更

--- js/main.js
+++ js/main.js
@@ -1,18 +1,7 @@
 const app = Vue.createApp({
     data: () => ({
-        km: 0,
-        m: 0
-    }),
-    watch: {
-        // 値が変更されたら、走る処理
-        km: function (value) {
-            this.km = value
-            this.m = value * 1000
-        },
-        m: function (value) {
-            this.km = value / 1000
-            this.m = value
-        }
-    }
+        isLarge: false,
+        hasError: true
+    })
     })
 app.mount('#app')
